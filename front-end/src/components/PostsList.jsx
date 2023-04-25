e9c8b8d0a9266b30992adcd9558566775808681a
Mudit Mittal <muditmittal@Mudits-MacBook-Air.local>
2023-04-25 17:31:02 -0400

    added routes nav and links

--- front-end/src/components/PostsList.jsx
+++ front-end/src/components/PostsList.jsx
@@ -1,47 +1,17 @@
-import { useEffect } from "react";
-import { useState } from "react";
-import Modal from "./modal";
-import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
+import { useLoaderData } from 'react-router-dom';
 
-function PostsList({ isPostingModal, onStopPostingModal }) {
-    const [posts, setPosts] = useState([]);
+function PostsList({}) {
 
-    useEffect(()=>{
-        async function fetchPosts() {
-            const response = await fetch('http://localhost:8080/posts');
-            const resData = await response.json();
-            setPosts(resData.posts);
-        }
-        fetchPosts();
-    }, []);
-
-    function addPostHandler(postData) {
-        fetch('http://localhost:8080/posts', {
-            method: 'POST',
-            body: JSON.stringify(postData),
-            headers: {
-                'Content-Type': 'application/json' 
-            }
-        });
-        setPosts((existingPosts) => [postData, ...existingPosts]);
-    }
+    const posts = useLoaderData();
 
     return (
         <>
-            {isPostingModal && (
-                <Modal onClose={onStopPostingModal}>
-                    <NewPost
-                        onCancel={onStopPostingModal}
-                        onAddPost={addPostHandler}
-                    />
-                </Modal>
-            )}
             {posts.length > 0 && (
             <ul className={classes.posts}>
                 {posts.map((post) => (
-                <Post key={Math.random()} author={post.author} body={post.body} />
+                <Post key={post.id} id={post.id} author={post.author} body={post.body} />
                 ))}
             </ul>
             )}
---
+++ front-end/src/routes/NewPost.jsx
@@ -0,0 +1,43 @@
+import { Link, Form, redirect } from 'react-router-dom';
+
+import Modal from '../components/modal';
+import classes from './NewPost.module.css';
+
+function NewPost() {
+    return (
+        <Modal>
+            <Form method='post' className={classes.form}>
+                <p>
+                    <label htmlFor="body">Text</label>
+                    <textarea id="body" name='body' required rows={3} />
+                </p>
+                <p>
+                    <label htmlFor="author">Your name</label>
+                    <input type="text" id="author" name='author' required />
+                </p>
+                <p className={classes.actions}>
+                    <Link to={'/'}>
+                        Cancel
+                    </Link>
+                    <button>Submit</button>
+                </p>
+            </Form>
+        </Modal>
+    );
+}
+
+export default NewPost;
+
+export async function action({ request }){
+    const formData = await request.formData();
+    const postData = Object.fromEntries(formData);
+
+    await fetch('http://localhost:8080/posts', {
+            method: 'POST',
+            body: JSON.stringify(postData),
+            headers: {
+                'Content-Type': 'application/json' 
+            }
+    });
+    return redirect('/');
+}
